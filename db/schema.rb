37712959d0be3980195ab972941d605fdef3bb8a
Teo Paolucci <teo.paolucci@gmail.com>
2015-11-10 09:53:37 -0500

    Created game model + migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150806195336) do
+ActiveRecord::Schema.define(version: 20151110144837) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "books", force: :cascade do |t|
-    t.string   "title"
-    t.string   "isbn"
-    t.integer  "user_id"
+  create_table "games", force: :cascade do |t|
+    t.integer  "discs"
+    t.integer  "time"
+    t.integer  "moves"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
